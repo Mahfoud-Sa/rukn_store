4463a6af9bf161e59d9b547a85df3a14e1d76ea6
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-07-01 17:07:25 +0300

    compelete some of the code

--- lib/app/features/home/presentation/pages/homepage.dart
+++ lib/app/features/home/presentation/pages/homepage.dart
@@ -19,16 +19,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final dio = Dio();
 
-  Future<List<String>> getHttp() async {
-    final response =
-        await dio.get('https://fakestoreapi.com/products/categories');
+  Future<List<dynamic>> getHttp() async {
+    var response = await dio.get('https://fakestoreapi.com/products/');
+    print(response);
 
-    final category = response
-        .toString()
-        .substring(1, response.toString().length - 1)
-        .split(',');
-    print(category);
-    return category;
+    final products =
+        response.data.map((json) => Product.fromJson(json)).toList();
+    // response
+    //     .toString()
+    //     .substring(1, response.toString().length - 1)
+    //     .split(',');
+    print(products);
+    return products;
   }
 
   @override
@@ -41,104 +43,70 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            //category Section
-            SizedBox(
-              height: 120,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        print('object');
-                      });
-                    },
-                    child: CategoryBtn(
-                      icon: Icons.woman,
-                    ),
+      body: Column(
+        children: [
+          //category Section
+          SizedBox(
+            height: 120,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      print('object');
+                    });
+                  },
+                  child: CategoryBtn(
+                    icon: Icons.woman,
                   ),
-                  CategoryBtn(
-                    icon: Icons.man,
-                  ),
-                  CategoryBtn(
-                    icon: Icons.abc_sharp,
-                  ),
-                  CategoryBtn(
-                    icon: Icons.join_full_outlined,
-                  ),
-                  CategoryBtn(
-                    icon: Icons.electric_bike,
-                  ),
-                ],
-              ),
+                ),
+                CategoryBtn(
+                  icon: Icons.man,
+                ),
+                CategoryBtn(
+                  icon: Icons.abc_sharp,
+                ),
+                CategoryBtn(
+                  icon: Icons.join_full_outlined,
+                ),
+                CategoryBtn(
+                  icon: Icons.electric_bike,
+                ),
+              ],
             ),
-
-            SizedBox(
-              height: 400,
-              child: ListView(
-                padding: const EdgeInsets.all(15),
-                children: [
-                  // FutureBuilder(
-                  //   future: getHttp(),
-                  //   builder: (context, snapshot) {
-                  //     if (snapshot.connectionState == ConnectionState.done) {
-                  //       if (snapshot.hasError) {
-                  //         return const Center(child: Text('خطاء في الاتصال بالانترنت'));
-                  //       }
-                  //       return SizedBox(
-                  //         height: 300,
-                  //         child: ListView.builder(
-                  //           scrollDirection: Axis.horizontal,
-                  //           itemCount: 4,
-                  //           itemBuilder: (context, index) {
-                  //             return Column(
-                  //               children: [
-                  //                 Container(
-                  //                   margin: EdgeInsets.all(10),
-                  //                   height: 50,
-                  //                   width: 50,
-                  //                   decoration: BoxDecoration(
-                  //                     image: DecorationImage(
-                  //                       image: AssetImage("assets/icons/women.png"),
-                  //                       fit: BoxFit.cover,
-                  //                     ),
-                  //                     borderRadius: BorderRadius.circular(50),
-                  //                     border: Border.all(color: Colors.black, width: 2),
-                  //                   ),
-                  //                 ),
-                  //                 Text(snapshot.data![index])
-                  //               ],
-                  //             );
-                  //           },
-                  //         ),
-                  //       );
-                  //     }
-                  //     return Center(child: CircularProgressIndicator());
-                  //   },
-                  // ),
-
-                  Column(
-                    children: [
-                      ItemWidget(
+          ),
+          //Items section
+          FutureBuilder(
+            future: getHttp(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return const Center(child: Text('خطاء في الاتصال بالانترنت'));
+                }
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return ItemWidget(
                         product: Product(
-                            title: 'Fits 15 Lapds Expanded ',
-                            price: 250,
-                            description: "padded sleeve,",
-                            category: 'Men close',
-                            image:
-                                "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-                            rating: Rating(rate: 5, count: 30)),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+                          title: 'Fits 15 Lapds Expanded ',
+                          // price: 250,
+                          // description: "padded sleeve,",
+                          // category: 'Men close',
+                          // image:
+                          //     "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                          // //  rating: Rating(rate: 5, count: 30)
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        ],
       ),
     );
   }
@@ -215,19 +183,20 @@ class ItemWidget extends StatelessWidget {
                 overflow: TextOverflow.ellipsis,
               ),
               Text(
-                product.description!,
+                "product.description!",
                 maxLines: 2,
               ),
-              Text(product.price!.toString()),
-              Text(product.rating!.rate.toString()),
+              Text("product.price!.toString()"),
+              Text("product.rating!.rate.toString()"),
             ],
           ),
           Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              padding: const EdgeInsets.all(15),
-              //  margin: EdgeInsets.all(3),
-              child: Image.network(product.image!)),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            padding: const EdgeInsets.all(15),
+            //  margin: EdgeInsets.all(3),
+            // child: Image.network(product.image!)
+          ),
         ],
       ),
     );
